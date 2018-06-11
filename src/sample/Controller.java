67a969993fd2549921484e2530e26789c9b69594
hanansson <nielsh78@yahoo.de>
2018-06-11 20:40:03 +0200

    Produkte bleiben gespeichert.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,11 +10,10 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -41,6 +40,48 @@ public class Controller implements Initializable {
         TableColumn artRow = new TableColumn("Art");
         tabelle.getColumns().addAll( nameRow, artRow);
 
+        BufferedReader br = null;
+        JSONParser parser = new JSONParser();
+
+        try {
+
+            String s;
+            br = new BufferedReader(new FileReader("Vorratsliste.json"));
+
+            while ((s = br.readLine()) != null) {
+
+                Produkt produkt = new Produkt("Name", "Art");
+                //Produkt muss in der Schleife initialisiert werden!
+
+                try {
+                    produktJ = (JSONObject) parser.parse(s);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                String name1 = (String) produktJ.get("name");
+                String art1 = (String) produktJ.get("art");
+
+                produkt.setName(name1);
+                produkt.setArt(art1);
+                vorratP.add(produkt);
+
+            }
+
+        } catch(IOException e){
+            e.printStackTrace();
+        } finally{
+            try {
+                if (br != null) br.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+
+        final ObservableList<Produkt> vorratO1 = FXCollections.observableArrayList(vorratP);
+        tabelle.setItems(vorratO1);
+
         //Fügt Kolumnen mit Titel hinzu.
 
         nameRow.setCellValueFactory(new PropertyValueFactory<Produkt, String>("Name"));
@@ -50,20 +91,22 @@ public class Controller implements Initializable {
     public void produktHinzufuegen (ActionEvent event) throws IOException {
 
         Produkt produkt = new Produkt("Name", "Art");
+        //Produkt muss in der Methode initialisiert werden!
         produkt.setName(nameEingabe.getText());
         produkt.setArt(artEingabe.getText());
         vorratP.add(produkt);
 
         //JSONObject produktJ = new JSONObject();
         String name = nameEingabe.getText();
-        String art = nameEingabe.getText();
+        String art = artEingabe.getText();
         produktJ.put("name", name);
         produktJ.put("art", art);
 
-        File f = new File("Vorratslistehurp.json");
+
+        File f = new File("Vorratsliste.json");
         if (f.exists()) {
 
-            FileWriter fw = new FileWriter("Vorratslistehurp.json", true);
+            FileWriter fw = new FileWriter("Vorratsliste.json", true);
             BufferedWriter bw = new BufferedWriter(fw);
             bw.newLine();
             bw.write(produktJ.toJSONString());
@@ -71,7 +114,7 @@ public class Controller implements Initializable {
 
         } else {
 
-            FileWriter fw = new FileWriter("Vorratslistehurp.json", true);
+            FileWriter fw = new FileWriter("Vorratsliste.json", true);
             fw.write(produktJ.toJSONString());
             fw.close();
 
